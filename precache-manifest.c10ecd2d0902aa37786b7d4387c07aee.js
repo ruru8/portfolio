7777bb4b2e1f05483d1e0b1c479234096ae68e4a
ruru8 <chiharu.yasuda@classi.jp>
2020-09-21 04:20:07 +0900

    Updates

---
+++ precache-manifest.c10ecd2d0902aa37786b7d4387c07aee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe253288c2b96f844281",
+    "url": "css/app.493cabca.css"
+  },
+  {
+    "revision": "28bfd49cacef3e2cfb69c361bd302184",
+    "url": "img/my.28bfd49c.png"
+  },
+  {
+    "revision": "61a40e1595cce39d76bb398e78528bac",
+    "url": "index.html"
+  },
+  {
+    "revision": "fe253288c2b96f844281",
+    "url": "js/app.df003090.js"
+  },
+  {
+    "revision": "a03e71363bd29914f361",
+    "url": "js/chunk-vendors.7e87eccb.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
